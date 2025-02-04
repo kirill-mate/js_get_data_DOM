320a251102585760a5525943445cf07c92919ff6
Kirill <Face2404@ukr.net>
2025-02-04 23:44:10 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write your code here
+const allPopulations = document.getElementsByClassName('population');
+const total = document.getElementsByClassName('total-population');
+const average = document.getElementsByClassName('average-population');
+const populationsArr = [];
+let sum = 0;
+
+for (const population of allPopulations) {
+  populationsArr.push(+population.textContent.replaceAll(',', ''));
+}
+
+sum = populationsArr.reduce((accum, pop) => accum + pop);
+
+const averageValue = Math.floor(sum / populationsArr.length);
+
+total[0].textContent = sum.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+average[0].textContent = averageValue
+  .toString()
+  .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
